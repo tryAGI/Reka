5a95835ed4137f8e6a8fb93e5e9f757693817eca
Konstantin S. <havendv@gmail.com>
2026-08-14 19:57:04 +0400

    feat: Updated OpenAPI spec (#39)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Reka/Generated/Reka.ModelsClient.g.cs
+++ src/libs/Reka/Generated/Reka.ModelsClient.g.cs
@@ -34,10 +34,17 @@ namespace Reka
 
         /// <inheritdoc/>
         public global::Reka.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Reka.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Reka.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
